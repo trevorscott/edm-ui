d21c81c05213cf499908d2e7efa7e6f7b6bf20bc
Trevor Scott <tscott@heroku.com>
2018-11-06 14:18:16 -0800

    multi-topic support

--- src/Home.js
+++ src/Home.js
@@ -9,26 +9,60 @@ class Home extends Component {
     this.state = {
       testvar1: null
     };
-    this.produceMessage = this.produceMessage.bind(this);
+    this.produceClickMessage = this.produceClickMessage.bind(this);
   }
 
-  formatMessage(id) {
+  formatMessage(topic, properties) {
     const json = {
-      "event_name": `${id}`,
-      "event": "button-click",
+      "topic": topic,
       "event_timestamp": Date.now(),
-      "properties": {}
+      "properties": properties
     }
     return json;
   }
 
-  produceMessage(e) {
+  produceClickMessage(e) {
     console.log(e.target.id);
+    const properties = {
+      "button_id": `${e.target.id}`,
+      "description": "button click"
+    }
 
     // format message
-    const msg = this.formatMessage(e.target.id);
+    const msg = this.formatMessage("edm-ui-click", properties);
+
     // send message
-    fetch(`${API_ROOT}/produceMessage`, {
+    fetch(`${API_ROOT}/produceClickMessage`, {
+      method: "POST",
+      body: JSON.stringify(msg),
+      headers: {
+        Accept: 'application/json',
+        origin: window.location.hostname,
+        'Content-Type': 'application/json',
+      }
+    }).then(function(response) {
+      // make sure you get a successful response
+      const res = response.json();
+      console.log(res);
+      return res;
+    }, function(error) {
+      // if not successful, retry
+      console.error(error.message); //=> String
+    })
+  }
+
+  producePageLoad() {
+    const properties = {
+      "user_agent": `${navigator.userAgent}`,
+      "description": "fix coffee page page loaded",
+      "url": window.location.origin
+    }
+
+    // format message
+    const msg = this.formatMessage("edm-ui-pageload", properties);
+
+    // send message
+    fetch(`${API_ROOT}/producePageLoad`, {
       method: "POST",
       body: JSON.stringify(msg),
       headers: {
@@ -48,7 +82,7 @@ class Home extends Component {
   }
 
   componentDidMount() {
-    // call API for page load
+    this.producePageLoad();
   }
 
   render() {
@@ -68,8 +102,8 @@ class Home extends Component {
           </div>
           <div className="masthead-elements-row-2">
             <div className="element-middle">
-              <button id="browse-kits" onClick={this.produceMessage} className="btn btn-default" href="#">Browse Kits</button>
-              <button id="browse-products" onClick={this.produceMessage} className="btn btn-trans" href="#">Browse Products</button>
+              <button id="browse-kits" onClick={this.produceClickMessage} className="btn btn-default" href="#">Browse Kits</button>
+              <button id="browse-products" onClick={this.produceClickMessage} className="btn btn-trans" href="#">Browse Products</button>
             </div>
           </div>
         </header>
@@ -80,25 +114,25 @@ class Home extends Component {
               <img className="product-image" src="images/products/071715_Heroku_3270-.jpg" alt="product photo" />
               <h3 className="product-name">AeroPress Coffee Maker</h3>
               <div className="product-price">$29.99</div>
-              <div className="product-button"><button id="areopress-coffee-maker" onClick={this.produceMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
+              <div className="product-button"><button id="areopress-coffee-maker" onClick={this.produceClickMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
             </li>
             <li>
               <img className="product-image" src="images/products/071715_Heroku_3263-.jpg" alt="product photo" />
               <h3 className="product-name">MSR Reactor 2.5L Stove System</h3>
               <div className="product-price">$159.99</div>
-              <div className="product-button"><button id="msr-reactor-2.5l-stove-system" onClick={this.produceMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
+              <div className="product-button"><button id="msr-reactor-2.5l-stove-system" onClick={this.produceClickMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
             </li>
             <li>
               <img className="product-image" src="images/products/071715_Heroku_3209-.jpg" alt="product photo" />
               <h3 className="product-name">Bunum Wo AA, Papua New Guinea</h3>
               <div className="product-price">$21.99</div>
-              <div className="product-button"><button id="bunum-wo-aa,-papua-new-guinea" onClick={this.produceMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
+              <div className="product-button"><button id="bunum-wo-aa,-papua-new-guinea" onClick={this.produceClickMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
             </li>
             <li>
               <img className="product-image" src="images/products/071715_Heroku_3302.jpg" alt="product photo" />
               <h3 className="product-name">Chemex Classic 6-cup Coffee Maker</h3>
               <div className="product-price">$39.99</div>
-              <div className="product-button"><button id="chemex-classic-6-cup-coffee-maker" onClick={this.produceMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
+              <div className="product-button"><button id="chemex-classic-6-cup-coffee-maker" onClick={this.produceClickMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
             </li>
           </ul>
         </section>
@@ -109,31 +143,31 @@ class Home extends Component {
               <img className="kit-image" src="images/kits/071715_Heroku_3338-Edit-.jpg" alt="product photo" />
               <h3 className="kit-name">The Chemex Kit</h3>
               <div className="kit-price">$396.96</div>
-              <div className="kit-button"><button id="the-chemex-kit" onClick={this.produceMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
+              <div className="kit-button"><button id="the-chemex-kit" onClick={this.produceClickMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
             </li>
             <li>
               <img className="kit-image" src="images/kits/071715_Heroku_3353-Edit-.jpg" alt="product photo" />
               <h3 className="kit-name">The Pour Over Kit</h3>
               <div className="kit-price">$171.96</div>
-              <div className="kit-button"><button id="the-pour-over-kit" onClick={this.produceMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
+              <div className="kit-button"><button id="the-pour-over-kit" onClick={this.produceClickMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
             </li>
             <li>
               <img className="kit-image" src="images/kits/071715_Heroku_3346-.jpg" alt="product photo" />
               <h3 className="kit-name">The Camper’s Kit</h3>
               <div className="kit-price">$272.95</div>
-              <div className="kit-button"><button id="the-camper's-kit" onClick={this.produceMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
+              <div className="kit-button"><button id="the-camper's-kit" onClick={this.produceClickMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
             </li>
             <li>
               <img className="kit-image" src="images/kits/071715_Heroku_3376-Edit-.jpg" alt="product photo" />
               <h3 className="kit-name">The French Press Kit</h3>
               <div className="kit-price">$219.94</div>
-              <div className="kit-button"><button id="the-french-press-kit" onClick={this.produceMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
+              <div className="kit-button"><button id="the-french-press-kit" onClick={this.produceClickMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
             </li>
             <li>
               <img className="kit-image" src="images/kits/071715_Heroku_3385-.jpg" alt="product photo" />
               <h3 className="kit-name">The Cold Brew Kit</h3>
               <div className="kit-price">$133.96</div>
-              <div className="kit-button"><button id="the-cold-brew-kit" onClick={this.produceMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
+              <div className="kit-button"><button id="the-cold-brew-kit" onClick={this.produceClickMessage} className="btn btn-hot" href="#">Add to Cart</button></div>
             </li>
           </ul>
         </section>
@@ -145,20 +179,20 @@ class Home extends Component {
           </div>
           <div className="trailer-links">
             <ul className="internal-links">
-              <li><span href="#" id="about" onClick={this.produceMessage}>About</span></li>
-              <li><span href="#" id="support" onClick={this.produceMessage}>Support</span></li>
-              <li><span href="#" id="contact-us" onClick={this.produceMessage}>Contact Us</span></li>
+              <li><span href="#" id="about" onClick={this.produceClickMessage}>About</span></li>
+              <li><span href="#" id="support" onClick={this.produceClickMessage}>Support</span></li>
+              <li><span href="#" id="contact-us" onClick={this.produceClickMessage}>Contact Us</span></li>
             </ul>
             <ul className="social-links">
-              <li><span id="twitter" href="#" onClick={this.produceMessage}>
+              <li><span id="twitter" href="#" onClick={this.produceClickMessage}>
                   <img className="social-logo" src="images/social/twitter.png" alt="twitter" />
                   <span className="social-verb">Follow on</span>
                   <span className="social-name">Twitter</span></span></li>
-              <li><span id="facebook" href="#" onClick={this.produceMessage}>
+              <li><span id="facebook" href="#" onClick={this.produceClickMessage}>
                   <img className="social-logo" src="images/social/facebook.png" alt="facebook" />
                   <span className="social-verb">Like Us on</span>
                   <span className="social-name">Facebook</span></span></li>
-              <li><span id="instagram" href="#" onClick={this.produceMessage}>
+              <li><span id="instagram" href="#" onClick={this.produceClickMessage}>
                   <img className="social-logo" src="images/social/instagram.png" alt="instagram" />
                   <span className="social-verb">Follow on</span>
                   <span className="social-name">Instagram</span></span></li>
