c2aad049aceb4768399f2bc1e858c6ee83bae11f
Trevor Scott <tscott@salesforce.com>
2018-11-19 14:22:47 -0800

    fix default kafka topics

--- src/kafka-topics.js
+++ src/kafka-topics.js
@@ -1,2 +1,21 @@
-export const CLICK_KAFKA_TOPIC     = process.env.REACT_APP_CLICK_KAFKA_TOPIC || 'edm-ui-click,';
-export const PAGE_LOAD_KAFKA_TOPIC = process.env.REACT_APP_PAGE_LOAD_KAFKA_TOPIC || 'edm-ui-pageload';
+import { API_ROOT } from './api-config';
+
+let defaultClickTopicName;
+let defaultPageLoadTopicName;
+
+if(API_ROOT == "http://localhost:5000") {
+  defaultClickTopicName = 'edm-ui-click-local'
+  defaultPageLoadTopicName 'edm-ui-pageload-local'
+} else {
+  defaultClickTopicName = 'edm-ui-click'
+  defaultPageLoadTopicName 'edm-ui-pageload'
+}
+
+const CLICK_KAFKA_TOPIC     = process.env.REACT_APP_CLICK_KAFKA_TOPIC || defaultClickTopicName;
+const PAGE_LOAD_KAFKA_TOPIC = process.env.REACT_APP_PAGE_LOAD_KAFKA_TOPIC || defaultPageLoadTopicName;
+
+
+module.exports = {
+  CLICK_KAFKA_TOPIC,
+  PAGE_LOAD_KAFKA_TOPIC
+}
