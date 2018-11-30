f1268b013080677f1765624a7549645a503051f3
Trevor Scott <tscott@salesforce.com>
2018-11-30 14:23:50 -0800

    switch to runtime env vars

--- src/api-config.js
+++ src/api-config.js
@@ -1,1 +1,3 @@
-export const API_ROOT = process.env.REACT_APP_EDM_RELAY_BACKEND_HOST || 'http://localhost:5000';
+import runtimeEnv from '@mars/heroku-js-runtime-env';
+const env = runtimeEnv();
+export const API_ROOT = env.REACT_APP_EDM_RELAY_BACKEND_HOST || 'http://localhost:5000';
